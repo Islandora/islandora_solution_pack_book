c6619c48725bcc8950f342bc2e8a4f3474c0cdb4
Nelson Hart <nelson.hart@gmail.com>
2013-06-15 11:04:52 -0300

    move externally refenced files to module

--- pageload/loader.php
+++ pageload/loader.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-
+<?php $path = $_GET['path']; ?>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -7,15 +7,15 @@
     <meta name="description" content="File Upload widget with multiple file selection, drag&amp;drop support, progress bar and preview images for jQuery. Supports cross-domain, chunked and resumable file uploads. Works with any server-side platform (Google App Engine, PHP, Python, Ruby on Rails, Java, etc.) that supports standard HTML form file uploads.">
     <meta name="viewport" content="width=device-width">
     <!-- Bootstrap CSS Toolkit styles -->
-    <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo "$path/css/bootstrap.min.css"; ?>">
     <!-- Generic page styles -->
     <link rel="stylesheet" href="css/style.css">
     <!-- Bootstrap styles for responsive website layout, supporting different screen sizes -->
-    <link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="<?php echo "$path/css/bootstrap-responsive.min.css"; ?>">
     <!-- Bootstrap CSS fixes for IE6 -->
-    <!--[if lt IE 7]><link rel="stylesheet" href="http://blueimp.github.com/cdn/css/bootstrap-ie6.min.css"><![endif]-->
+    <!--[if lt IE 7]><link rel="stylesheet" href="<?php echo "$path/css/bootstrap-ie6.min.css"; ?>"><![endif]-->
     <!-- Bootstrap Image Gallery styles -->
-    <link rel="stylesheet" href="http://blueimp.github.com/Bootstrap-Image-Gallery/css/bootstrap-image-gallery.min.css">
+    <link rel="stylesheet" href="<?php echo "$path/css/bootstrap-image-gallery.min.css"; ?>">
     <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
     <link rel="stylesheet" href="css/jquery.fileupload-ui.css">
     <!-- Shim to make HTML5 elements usable in older Internet Explorer versions -->
@@ -158,14 +158,14 @@
     <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
     <script src="js/vendor/jquery.ui.widget.js"></script>
     <!-- The Templates plugin is included to render the upload/download listings -->
-    <script src="http://blueimp.github.com/JavaScript-Templates/tmpl.min.js"></script>
+    <script src="<?php echo "$path/js/tmpl.min.js"; ?>"></script>
     <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
-    <script src="http://blueimp.github.com/JavaScript-Load-Image/load-image.min.js"></script>
+    <script src="<?php echo "$path/js/load-image.min.js"; ?>"></script>
     <!-- The Canvas to Blob plugin is included for image resizing functionality -->
-    <script src="http://blueimp.github.com/JavaScript-Canvas-to-Blob/canvas-to-blob.min.js"></script>
+    <script src="<?php echo "$path/js/canvas-to-blob.min.js"; ?>"></script>
     <!-- Bootstrap JS and Bootstrap Image Gallery are not required, but included for the demo -->
-    <script src="http://blueimp.github.com/cdn/js/bootstrap.min.js"></script>
-    <script src="http://blueimp.github.com/Bootstrap-Image-Gallery/js/bootstrap-image-gallery.min.js"></script>
+    <script src="<?php echo "$path/js/bootstrap.min.js"; ?>"></script>
+    <script src="<?php echo "$path/js/bootstrap-image-gallery.min.js"; ?>"></script>
     <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
     <script src="js/jquery.iframe-transport.js"></script>
     <!-- The basic File Upload plugin -->
