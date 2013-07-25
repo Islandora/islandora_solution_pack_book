960f730b1c96c054256dce5e1bf7ebeec4ff0cce
DGI <dev@discoverygarden.ca>
2013-07-25 16:25:44 -0300

    Added meta tables and Schema for SEO

--- theme/islandora-book.tpl.php
+++ theme/islandora-book.tpl.php
@@ -9,4 +9,22 @@
     <?php print $viewer; ?>
   </div>
 <?php endif; ?>
-<!-- @todo Add table of metadata values -->
+<div class="islandora-book clearfix" itemscope itemtype="http://schema.org/Book">
+	<fieldset class="collapsible collapsed islandora-book-metadata">
+	  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+	    <div class="fieldset-wrapper">
+	      <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-book-fields">
+	        <?php $row_field = 0; ?>
+	        <?php foreach($dc_array as $key => $value): ?>
+	          <dt property="<?php print $value['dcterms']; ?>" content="<?php print $value['value']; ?>" class="<?php print $value['class']; ?><?php print $row_field  == 0 ? ' first' : ''; ?>">
+	            <?php print $value['label']; ?>
+	          </dt>
+	          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
+	            <?php print $value['value']; ?>
+	          </dd>
+	          <?php $row_field++; ?>
+	        <?php endforeach; ?>
+	      </dl>
+	    </div>
+	</fieldset>
+</div>
