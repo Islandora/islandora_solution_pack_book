a01486d590376d5428d44556c08d749d8515dcd0
nruest <ruestn@gmail.com>
2015-02-13 10:35:48 -0500

    Address ISLANDORA-1191

--- theme/islandora-book-book.tpl.php
+++ theme/islandora-book-book.tpl.php
@@ -4,24 +4,25 @@
  * Template file to style output.
  */
 ?>
-<?php if (isset($viewer)): ?>
+<?php if(isset($viewer)): ?>
   <div id="book-viewer">
     <?php print $viewer; ?>
   </div>
 <?php endif; ?>
 
-<div class="islandora-book-metadata">
-   <?php print $description; ?>
-    <?php if ($parent_collections): ?>
-      <div>
-        <h2><?php print t('In collections'); ?></h2>
-        <ul>
-          <?php foreach ($parent_collections as $collection): ?>
-            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
-    <?php print $metadata; ?>
-</div>
-<!-- @todo Add table of metadata values -->
+<?php if($display_metadata === 1): ?>
+  <div class="islandora-book-metadata">
+    <?php print $description; ?>
+    <?php if($parent_collections): ?>
+  <div>
+    <h2><?php print t('In collections'); ?></h2>
+    <ul>
+      <?php foreach ($parent_collections as $collection): ?>
+        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+  <?php endif; ?>
+  <?php print $metadata; ?>
+  </div>
+<?php endif; ?>
